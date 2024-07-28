fa5b2c58ea54c6ade4e1d6e669712b5f0289101f
Noavaran Khallagh Iranian <106599493+Amirreza086@users.noreply.github.com>
2024-07-28 14:53:31 +0330

    Update SplashScreen.tsx

--- src/app/components/splash-screen/SplashScreen.tsx
+++ src/app/components/splash-screen/SplashScreen.tsx
@@ -21,7 +21,7 @@ export function SplashScreen({ children }: SplashScreenProps) {
         justifyContent="Center"
       >
         <Text size="H2" align="Center">
-          Otagh
+          پیام رسان اتاق
         </Text>
       </Box>
     </Box>
