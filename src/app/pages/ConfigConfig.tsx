f03811c3ae5eb476d223633fc9c9ca83a1509739
Noavaran Khallagh Iranian <106599493+Amirreza086@users.noreply.github.com>
2024-07-28 20:12:24 +0330

    Update ConfigConfig.tsx

--- src/app/pages/ConfigConfig.tsx
+++ src/app/pages/ConfigConfig.tsx
@@ -7,7 +7,7 @@ export function ConfigConfigLoading() {
     <SplashScreen>
       <Box grow="Yes" direction="Column" gap="400" alignItems="Center" justifyContent="Center">
         <Spinner variant="Secondary" size="600" />
-        <Text>Heating up Otagh</Text>
+        <Text>در حال بارگزاری</Text>
       </Box>
     </SplashScreen>
   );
@@ -25,7 +25,7 @@ export function ConfigConfigError({ error, retry, ignore }: ConfigConfigErrorPro
         <Dialog>
           <Box style={{ padding: config.space.S400 }} direction="Column" gap="400">
             <Box direction="Column" gap="100">
-              <Text>Failed to load client configuration file.</Text>
+              <Text>فایل پیکربندی بارگیری نشد</Text>
               {typeof error === 'object' &&
                 error &&
                 'message' in error &&
@@ -37,12 +37,12 @@ export function ConfigConfigError({ error, retry, ignore }: ConfigConfigErrorPro
             </Box>
             <Button variant="Critical" onClick={retry}>
               <Text as="span" size="B400">
-                Retry
+                تلاش دوباره
               </Text>
             </Button>
             <Button variant="Critical" onClick={ignore} fill="Soft">
               <Text as="span" size="B400">
-                Continue
+                ادامه
               </Text>
             </Button>
           </Box>
