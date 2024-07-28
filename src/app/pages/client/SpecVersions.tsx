8be0b5fd0a7cc5805e143dd1381a55e890fc61e8
Noavaran Khallagh Iranian <106599493+Amirreza086@users.noreply.github.com>
2024-07-28 20:38:45 +0330

    Update SpecVersions.tsx

--- src/app/pages/client/SpecVersions.tsx
+++ src/app/pages/client/SpecVersions.tsx
@@ -12,7 +12,7 @@ export function SpecVersions({ baseUrl, children }: { baseUrl: string; children:
         <SplashScreen>
           <Box direction="Column" grow="Yes" alignItems="Center" justifyContent="Center" gap="400">
             <Spinner variant="Secondary" size="600" />
-            <Text>Connecting to server</Text>
+            <Text>در حال اتصال به سرور</Text>
           </Box>
         </SplashScreen>
       )}
@@ -22,16 +22,16 @@ export function SpecVersions({ baseUrl, children }: { baseUrl: string; children:
             <Dialog>
               <Box direction="Column" gap="400" style={{ padding: config.space.S400 }}>
                 <Text>
-                  Failed to connect to homeserver. Either homeserver is down or your internet.
+                  اتصال به سرور خانگی ناموفق بود. یا سرور خانه قطع است یا اینترنت شما
                 </Text>
                 <Button variant="Critical" onClick={retry}>
                   <Text as="span" size="B400">
-                    Retry
+                    تلاش دوباره
                   </Text>
                 </Button>
                 <Button variant="Critical" onClick={ignore} fill="Soft">
                   <Text as="span" size="B400">
-                    Continue
+                    ادامه
                   </Text>
                 </Button>
               </Box>
