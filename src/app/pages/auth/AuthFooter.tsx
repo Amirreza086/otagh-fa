e28b3d6357b6fc75a231a2f116edbb9ce398d263
Noavaran Khallagh Iranian <106599493+Amirreza086@users.noreply.github.com>
2024-07-28 21:09:59 +0330

    Update AuthFooter.tsx

--- src/app/pages/auth/AuthFooter.tsx
+++ src/app/pages/auth/AuthFooter.tsx
@@ -6,7 +6,7 @@ export function AuthFooter() {
   return (
     <Box className={css.AuthFooter} justifyContent="Center" gap="400" wrap="Wrap">
       <Text as="a" size="T300" href="https://www.1nr.ir/" rel="noreferrer">
-        Created By Amirreza Eskandarzadeh
+        ساخته شده توسط امیررضا اسکندرزاده
       </Text>
     </Box>
   );
